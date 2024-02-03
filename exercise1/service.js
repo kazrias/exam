407d84a6af1e37fa3852eef867a9ef8bacde3bda
Artyom Harutyunyan <ka6rias@gmail.com>
2024-02-03 14:40:51 +0400

    Create private fetchData method and public method to use it

--- exercise1/service.js
+++ exercise1/service.js
@@ -1,15 +1,31 @@
 class Service {
+  constructor() {
 
-  #fetchData(){
-    
+  }
+  #fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const data = await response.json();
+      return data
+    }
+    catch (error) {
+      console.log(`Fetching error: ${error.message}`);
+    }
+  }
+  fetchDataFromUrl(url) {
+    return this.#fetchData(url)
   }
 
-  static getInstance(name) {
+  static getInstance() {
     if (!this.instance) {
-      this.instance = new this(name);
+      this.instance = new this();
     }
     return this.instance;
   }
-  
+
 }
+
 
