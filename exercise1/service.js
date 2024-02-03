78e9ad2f42dcc83a9971fdca597ceac8f1664ea0
Artyom Harutyunyan <ka6rias@gmail.com>
2024-02-03 15:11:38 +0400

    Implemented groupBy method and getGroupedDataBy method

--- exercise1/service.js
+++ exercise1/service.js
@@ -2,6 +2,12 @@ class Service {
   constructor() {
 
   }
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new this();
+    }
+    return this.instance;
+  }
   #fetchData = async (url) => {
     try {
       const response = await fetch(url);
@@ -15,17 +21,61 @@ class Service {
       console.log(`Fetching error: ${error.message}`);
     }
   }
+
   fetchDataFromUrl(url) {
     return this.#fetchData(url)
   }
 
-  static getInstance() {
-    if (!this.instance) {
-      this.instance = new this();
-    }
-    return this.instance;
+  getGroupedDataBy(data, key) {
+    return data.groupBy(key)
   }
-
 }
+
+let mainService = Service.getInstance();
+
+
+async function getSomeData() {
+  let data = await mainService.fetchDataFromUrl('https://jsonplaceholder.typicode.com/posts');
+  console.log(data);
+  console.log(mainService.getGroupedDataBy(data, 'id'));
+}
+getSomeData()
+Array.prototype.groupBy = function (key) {
+  const res = {};
+  for (const obj of this) {
+    const resKey = obj[key];
+    if (!res[resKey]) {
+      res[resKey] = [];
+    }
+    res[resKey].push(obj);
+  }
+  return res;
+}
+
+// const users = [
+//   {
+//     name: 'Tyom',
+//     age: 23,
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Artur',
+//     age: 13,
+//     gender: 'male'
+//   },
+//   {
+//     name: 'Armin',
+//     age: 23,
+//     gender: 'female'
+//   },
+//   {
+//     name:"Anush",
+//     age:33,
+//     gender:'female'
+//   }
+// ]
+// console.log('gender', users.groupBy('gender'));
+// console.log('age', users.groupBy('age'));
+// console.log('name', users.groupBy('name'));
 
 
