734cd53df09e7531685877c5f5af000fa3e5668a
Artyom Harutyunyan <ka6rias@gmail.com>
2024-02-03 16:30:03 +0400

    Create Button component and Modal start

--- exercise2/src/App.jsx
+++ exercise2/src/App.jsx
@@ -1,10 +1,19 @@
-
+import { useState } from 'react';
 import './App.css';
-
+import { Button } from './components/Button/Button';
+import { Modal } from './components/Modal/Modal';
 function App() {
+  const [createIsClicked, setCreateIsClicked] = useState(false)
+  const [showIsClicked, setShowIsClicked] = useState(false)
   return (
     <div className="App">
-      
+      <div className="buttons">
+        <Button onClick={() => setCreateIsClicked(true)} type={'main'}>Create User</Button>
+        <Button onClick={() => setShowIsClicked(true)} type={'secondary'}>Show Users</Button>
+      </div>
+      {
+        createIsClicked && <Modal />
+      }
     </div>
   );
 }
