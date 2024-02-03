4ba4fd23460e896bb2090052963bea508a4d5fe1
Artyom Harutyunyan <ka6rias@gmail.com>
2024-02-03 14:26:56 +0400

    Create singleton Service class and private fecthFunc

--- exercise1/service.js
+++ exercise1/service.js
@@ -1,1 +1,15 @@
-test
+class Service {
+
+  #fetchData(){
+    
+  }
+
+  static getInstance(name) {
+    if (!this.instance) {
+      this.instance = new this(name);
+    }
+    return this.instance;
+  }
+  
+}
+
